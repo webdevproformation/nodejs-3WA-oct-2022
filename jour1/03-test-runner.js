fd97f189540088f27a9113caa77590b59b66d590
Malik <malik.h@webdevpro.net>
2022-10-24 10:40:05 +0200

    premier test unitaire

---
+++ jour1/03-test-runner.js
@@ -0,0 +1,33 @@
+// npm init --yes 
+
+/**
+dans package.json 
+"scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+},
+
+npm run test 
+
+lancer jest 
+
+"scripts": {
+    "test": "jest",
+    "test-old": "echo \"Error: no test specified\" && exit 1"
+},
+
+npm run test
+
+No tests found, exiting with code 1
+Run with `--passWithNoTests` to exit with code 0
+In C:\Users\harri\Desktop\nodejs\jour1
+//  5 files checked.
+//  testMatch: **/__tests__/**/*.[jt]s?(x), **/?(*.)+(spec|test).[tj]s?(x) - 0 matches
+//  testPathIgnorePatterns: \\node_modules\\ - 5 matches
+//  testRegex:  - 0 matches
+//Pattern:  - 0 matches
+
+/* dossier __tests__ qui contient des fichiers .js 
+fichier .test.js 
+fichier .test.ts
+fichier .test.jsx
+fichier .test.tsx */
---
+++ jour1/test/premier.test.js
@@ -0,0 +1,22 @@
+// npm run test 
+// message change encore 
+
+// tests
+//test()
+it( "texte qui décrit le test effectué" , () => {
+    // callback 
+    // effectuer des traitements 
+    const a = 1 ; // créer une variable 
+    // Assertion vérifier que une variable / traitement retourne bien une valeur attendue
+    expect(a).toBe(1) ; // Assertion 
+}  );
+
+/**
+ * npm run test
+PASS  test/premier.test.js
+  √ texte qui décrit le test effectué (2 ms)
+Tests:       1 passed, 1 total
+Snapshots:   0 total
+Time:        0.438 s
+Ran all test suites
+ */
