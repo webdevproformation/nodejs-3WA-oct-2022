d5b82d38cf84948afbaf91d1c3a722c07ba8a102
Malik <malik.h@webdevpro.net>
2022-10-24 09:47:13 +0200

    start

---
+++ jour1/01-test.js
@@ -0,0 +1,16 @@
+// lorsque l'on crée un projet PHP 
+// du Javascript (front ou back)
+// au fur et à mesure que l'on avance dans le projet
+// vérifier que l'ensemble du code que l'on écrit fonctionne 
+// et ne casse pas l'existant 
+
+// si le projet est simple (court quelques pages / fichiers)
+// => effectuer les tests et les vérifications manuellement
+
+// si le projet est complexe // si le projet est partagé entre plusieurs développeurs 
+
+// les tests deviennent de plus en plus fastidieux à réaliser
+
+
+// Le Testing => le fait d'automatiser le fait de tester son code 
+// écrire du code qui test son propre code 
