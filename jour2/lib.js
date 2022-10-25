d5b79ad663bf7f182b1f884772cb3809ed266841
Malik <malik.h@webdevpro.net>
2022-10-25 10:20:18 +0200

    fin test unitaire

--- jour2/lib.js
+++ jour2/lib.js
@@ -3,9 +3,14 @@
 exports.getArticles = ( texte , callback  ) => {
 
     return callback().then( reponse => reponse.data.filter(article => article.title.includes(texte)) )
-
 }
 
+
+exports.useApi = (callback) => {
+    return callback().then( reponse => {
+        return reponse.map( el => el.name)
+    })
+}
 
 
 
--- jour2/test/lib.test.js
+++ jour2/test/lib.test.js
@@ -1,4 +1,4 @@
-const { getArticles } = require("../lib")
+const { getArticles , useApi } = require("../lib")
 
 it("test unitaire de lib" , () => {
     const mockCallbackBackAsync = jest.fn();
@@ -13,4 +13,25 @@ it("test unitaire de lib" , () => {
     expect( getArticles( "Article" , mockCallbackBackAsync ) ).resolves.toEqual([ {title : "Article 1" , id : 1},
     {title : "Article 2" , id : 2} ]);
     // attendre que la promesse soit exécutée expect()  .toEqual()
+});
+
+// créer une fonction dans le fichier lib.js 
+// qui appelle une API qui retourne une collection 
+// [ { name : "...." , id : 1 , adresse : "...." } , { name : "...." , id : 1 , adresse : "...." }   ]
+// utiliser les fonctions de mock de jest pour tester que votre fonction retourne bien ce type de donnée
+
+
+it("exo 1" , () => {
+    const mockUseApi = jest.fn();
+    const resultatApi = [ 
+        { name : "Alain" , id : 1 , adresse : "75 rue de Paris" } , 
+        { name : "Benois" , id : 2 , adresse : "76 rue de Marseille" }   
+    ];
+    mockUseApi.mockReturnValue(Promise.resolve(resultatApi));
+
+    //assertion 
+    expect(useApi(mockUseApi)).resolves.toEqual( ["Alain" , "Benois"] );
+
 })
+
+
