5bc91678937c86b82a40637333115594b516f2ed
Malik <malik.h@webdevpro.net>
2022-10-26 16:01:57 +0200

    page d'accueil opérationnelle

--- jour3-front/route.js
+++ jour3-front/route.js
@@ -5,4 +5,16 @@ const path = require("path");
 module.exports = function(app){
 
     // paramètrage de pug 
+    app.set("views" , path.join(__dirname , "views"))
+    app.set("view engine" , "pug");
+    app.set("view cache" , process.env.NODE_ENV === "production");
+    // fichier css / js / image .. public
+    app.set(express.static(path.join(__dirname , "public")))
+
+    // fin des paramètrages de pug 
+
+    app.get("/" , (req, rep) => {
+        rep.render("index")
+    })
+
 }
