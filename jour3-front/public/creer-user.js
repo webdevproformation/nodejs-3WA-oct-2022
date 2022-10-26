41757159f7ca107a2b49c635293fa47168cd6368
Malik <malik.h@webdevpro.net>
2022-10-26 16:57:15 +0200

    page de création de profil utilisateur

---
+++ jour3-front/public/creer-user.js
@@ -0,0 +1,24 @@
+document.querySelector("form").addEventListener("submit" , (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const values = [...data.entries()]
+    const user = {
+        pseudo : values[0][1],
+        email : values[1][1],
+        password : values[2][1],
+    }
+    
+    const options = {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(user)
+    }
+    fetch("https://lit-island-18380.herokuapp.com/addUser" , options)
+            .then(reponse => reponse.json())
+            .then((data) => {
+                console.log(data);
+                
+            })
+})
--- jour3-front/route.js
+++ jour3-front/route.js
@@ -9,12 +9,27 @@ module.exports = function(app){
     app.set("view engine" , "pug");
     app.set("view cache" , process.env.NODE_ENV === "production");
     // fichier css / js / image .. public
-    app.set(express.static(path.join(__dirname , "public")))
-
+   // console.log(path.join(__dirname , "public"))
+    app.use(express.static( path.join(__dirname , "public")))
     // fin des paramètrages de pug 
 
     app.get("/" , (req, rep) => {
         rep.render("index")
     })
+    app.get("/creer-user" , (req, rep) => {
+        rep.render("creer-user")
+    })
+
+    // créer une autre page créer de profil utilisateur
+    // page vous avez un formulaire 
+    // 3 champs 
+    // pseudo
+    // email
+    // password 
+    // vérifier que tous les champs sont bien remplis 
+    // requête ajax vers l'api que l'on créé via un fetch()
+    // https://lit-island-18380.herokuapp.com/addUser
+    // go
+
 
 }
