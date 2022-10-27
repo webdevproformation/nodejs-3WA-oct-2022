f238d81c5bb08faa5c5511772f43ebdb09a2e13a
Malik <malik.h@webdevpro.net>
2022-10-27 12:01:46 +0200

    menu dynamique

---
+++ jour3-front/public/menu-top.js
@@ -0,0 +1,13 @@
+(function(){
+    const verif = localStorage.getItem("token");
+    if(verif == null) {
+        document.querySelectorAll(".connexion").forEach((el) => {
+            el.remove()
+        })
+    }else {
+        document.querySelectorAll(".deconnexion").forEach((el) => {
+            el.remove()
+        })
+    }
+})()
+
